1067c1e90fd23ec4960ed7bc4adddcdccfb09d2d
rogerdpack <rogerpack2005@gmail.com>
2009-07-07 12:48:45 -0600

    add class_eval

--- lib/remembered_evals.rb
+++ lib/remembered_evals.rb
@@ -1,37 +1,78 @@
 require 'digest/md5'
 require 'rubygems'
 require 'facets/file/write'
+require 'facets/file/read' # File.sanitize
 
-module Kernel
- alias :original_eval :eval
+class RememberedEval
 
 =begin rdoc
-
+ # method_name, string, binding, file, line [file and line are hereby ignored since ours supplant it]
  doctest: eval saves away files into some path, then eval's them from there
  >> eval "$a = 3"
- >> File.directory? '.remembered_evals'
+ >> File.directory? '._remembered_evals'
  => true
  >> $a
  => 3
 
- TODO work with class_eval, too
 
-
- Using it causes the backtraces to behave slightly differently
- >> eval "begin; raise; rescue Exception => e; $a = e; end"
- >> $a.backtrace[0].include? '.remembered_evals/'
- >> true
+ Using it causes the backtraces to behave slightly differently [I'd call it better]
+ >> eval "begin; raise; rescue Exception => e; $exception = e; end"
+ >> $exception.backtrace[0]
+ => "._remembered_evals/begin__raise__rescue_Exception__a48fc331d57ebd559347525d306d6a67:1:in `eval'"
 
 =end
 
- def eval *args # string, binding, file, line [file and line are hereby ignored since ours supplant it]
-  path = '.remembered_evals'
+ def self.cache_code code_string
+  path = '._remembered_evals'
   Dir.mkdir path unless File.directory? path
-  digest = Digest::MD5.hexdigest("Hello World\n")
-  fullpath = path + '/' + Digest::MD5.hexdigest(args[0])
-  saved = File.write(fullpath, args[0])
-  original_eval args[0], args[1], fullpath, 1
+  # create something like /code0xdeadbeef for filename
+  fullpath = path + '/' + File.sanitize(code_string[0..31]) + Digest::MD5.hexdigest(code_string)[0..63] # don't need too long here
+  saved = File.write(fullpath, code_string) # write it out
+  fullpath
  end
 
 end
+
+class Class
+ alias :original_class_eval :class_eval
+
+ def class_eval *args
+  if block_given? # this one is already sourced
+    original_class_eval { yield }
+  else
+    path = RememberedEval.cache_code args[0]
+    original_class_eval args[0], path, 1 # no binding here
+  end
+ end
+end
+
+
+class Object
+ alias :original_eval :eval
+ def eval *args
+   path = RememberedEval.cache_code args[0]
+   original_eval args[0], args[1], path, 1 # args[1] is binding, args[0] is the code, ignore args[2], 3, which we replace :)
+ end
+
+end
+
+=begin rdoc
+ doctest: work with mod, too
+ >> class A; end
+ >> A.class_eval "def go3; 3; end"
+ >> A.new.go3
+ => 3
+
+ it reverts back to normal [non-cached] behavior unless you pass it a binding [?]
+ >> a = 3
+ >> eval "a = 4"
+ >> a
+ => 4
+
+ >> module M; end
+ >> M.module_eval "def go; 3; end"
+ >> M.instance_methods
+ => ["go"]
+
+=end
 
